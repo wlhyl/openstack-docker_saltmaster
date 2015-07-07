3899be1cca89f3e1b54c1ef776af96d0468a7a58
lzh <1216083447@qq.com>
2015-07-07 16:43:32 +0800

    修改entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ fi
 if [ ! -f /etc/keystone/.complete ];then
     cp -rp /salt/* /etc/salt/
     sed -i /pillarHttp/s#http://127.0.0.1:8000/api/#${PILLAR_HTTP_ENDPOINT}#g /etc/salt/master
-    touch /etc/keystone/.complete
+    touch /etc/salt/.complete
 fi
 
 /usr/bin/supervisord -n
